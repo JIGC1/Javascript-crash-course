d4babe3544cfedc7c5aa57da3a8099b210e96da9
JIGC1 <JIGC1@PROTONMAIL.COM>
2022-04-28 01:35:36 -0500

    js

---
+++ home.js
@@ -0,0 +1,15 @@
+console.log('hello');
+//alert('hello This is JIGC');
+
+//variables
+
+var b = 'smoothie';
+
+console.log(b);
+
+var somenumber = 45;
+console.log(somenumber);
+
+var age = prompt('what is your age?');
+
+document.getElementById('someText').innerHTML = age;
